f7838f82467fc62261f9b420639598411462b21e
adrijan78 <adrijan728@gmail.com>
2020-04-11 18:53:50 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -3,18 +3,78 @@ class Point {
 	double x,y;
 	String boja;
 
-	//TODO add new variable
+	public Point(String id, double x, double y, String boja) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.boja = boja;
+	}
 
-	//TODO constructor
+	public String getId() {
+		return id;
+	}
 
-	//TODO setters and getters
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String getBoja() {
+		return boja;
+	}
+
+	public void setBoja(String boja) {
+		this.boja = boja;
+	}
+
+
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		if(xDirection =='L'){
+			setX(this.x-1);
+		}
+		if(xDirection =='R'){
+			setX(this.x+1);
+		}
+		if(yDirection =='U'){
+			setY(this.y+1);
+		}
+		if(yDirection =='D'){
+			setY(this.y-1);
+		}
+
 	}
 
 	public void draw () {
 		//TODO
+		if(x>0 && y>0){
+			System.out.println("Tockata se naogja vo I kvadrant");
+		}
+		if(x<0 && y>0){
+			System.out.println("Tockata se naogja vo II kvadrant");
+		}
+		if(x<0 && y<0){
+			System.out.println("Tockata se naogja vo III kvadrant");
+		}
+		if(x>0 && y<0){
+			System.out.println("Tockata se naogja vo IV kvadrant");
+		}
 	}
 
 
